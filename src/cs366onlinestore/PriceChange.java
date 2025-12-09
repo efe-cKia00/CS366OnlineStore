6b8035e313c9ede4806ede6e594d3310c92838b9
KalanLarson <kalan.larson@ndus.edu>
2025-12-09 14:35:31 -0600

    Created PriceChange Class

---
+++ src/cs366onlinestore/PriceChange.java
@@ -0,0 +1,83 @@
+
+package cs366onlinestore;
+
+import java.time.LocalDateTime;
+
+public class PriceChange {
+    // ---------- Fields (from UML) ----------
+    private int priceChangeId;
+    private int productId;
+    private float oldPrice;
+    private float newPrice;
+    private LocalDateTime changeDate;
+
+    // ---------- Constructors ----------
+    public PriceChange() {
+        // no-arg constructor
+    }
+
+    public PriceChange(int priceChangeId,
+                       int productId,
+                       float oldPrice,
+                       float newPrice,
+                       LocalDateTime changeDate) {
+
+        this.priceChangeId = priceChangeId;
+        this.productId = productId;
+        this.oldPrice = oldPrice;
+        this.newPrice = newPrice;
+        this.changeDate = changeDate;
+    }
+
+    // ---------- Getters / Setters ----------
+    public int getPriceChangeId() {
+        return priceChangeId;
+    }
+
+    public void setPriceChangeId(int priceChangeId) {
+        this.priceChangeId = priceChangeId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public float getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(float oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public float getNewPrice() {
+        return newPrice;
+    }
+
+    public void setNewPrice(float newPrice) {
+        this.newPrice = newPrice;
+    }
+
+    public LocalDateTime getChangeDate() {
+        return changeDate;
+    }
+
+    public void setChangeDate(LocalDateTime changeDate) {
+        this.changeDate = changeDate;
+    }
+
+    @Override
+    public String toString() {
+        return "PriceChange{" +
+                "priceChangeId=" + priceChangeId +
+                ", productId=" + productId +
+                ", oldPrice=" + oldPrice +
+                ", newPrice=" + newPrice +
+                ", changeDate=" + changeDate +
+                '}';
+    }
+}
